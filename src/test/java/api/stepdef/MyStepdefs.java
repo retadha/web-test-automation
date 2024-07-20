3ef8c239e085942b85930e843438a270a423aaae
retadha <retadha.rumi@gmail.com>
2024-07-20 11:43:40 +0700

    Initiate a dummy test

---
+++ src/test/java/api/stepdef/MyStepdefs.java
@@ -0,0 +1,16 @@
+package api.stepdef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import org.junit.jupiter.api.Assertions;
+
+public class MyStepdefs {
+    @Given("check first time")
+    public void checkFirstTime() {
+
+    }
+
+    @And("test succeed")
+    public void testSucceed() {
+    }
+}
