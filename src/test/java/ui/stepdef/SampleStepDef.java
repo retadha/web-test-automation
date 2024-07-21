4922165599898d5ea37e7b611ee4c3938425c328
retadha <retadha.rumi@gmail.com>
2024-07-21 13:20:30 +0700

    Initiate sample test

---
+++ src/test/java/ui/stepdef/SampleStepDef.java
@@ -0,0 +1,14 @@
+package ui.stepdef;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class SampleStepDef {
+    @When("sample test starts")
+    public void sampleTestStarts() {
+    }
+
+    @Then("test run")
+    public void testRun() {
+    }
+}
